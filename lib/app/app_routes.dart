9932a55c5c05b4229c7864c3ffba0557d2d73eb5
kaloqnnajdenov <40794986+kaloqnnajdenov@users.noreply.github.com>
2025-09-28 10:52:50 +0200

    Revert "Add Supabase authentication menu and pages"

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
-import '../presentation/pages/auth/login_page.dart';
-import '../presentation/pages/auth/profile_page.dart';
-import '../presentation/pages/auth/sign_up_page.dart';
 import '../presentation/pages/map_page.dart';
 
 class AppRoutes {
   static const String map = '/';
-  static const String login = '/login';
-  static const String signUp = '/sign-up';
-  static const String profile = '/profile';
 
   static Map<String, WidgetBuilder> get routes => {
         map: (_) => const MapPage(),
-        login: (_) => const LoginPage(),
-        signUp: (_) => const SignUpPage(),
-        profile: (_) => const ProfilePage(),
       };
 }
