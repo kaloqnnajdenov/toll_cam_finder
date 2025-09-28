b54031bbd0973ed7c3f42ced4cd2ac7906951fc6
kaloqnnajdenov <40794986+kaloqnnajdenov@users.noreply.github.com>
2025-09-28 10:53:54 +0200

    Add shared_preferences dependency for Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toll_cam_finder/services/auth_controller.dart';
 import 'package:toll_cam_finder/services/average_speed_est.dart';
+import 'package:toll_cam_finder/services/supabase_service.dart';
+
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SupabaseService.initialise();
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => AverageSpeedController(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AverageSpeedController()),
+        ChangeNotifierProvider(create: (_) => AuthController()),
+      ],
       child: const TollCamApp(),
     ),
   );
