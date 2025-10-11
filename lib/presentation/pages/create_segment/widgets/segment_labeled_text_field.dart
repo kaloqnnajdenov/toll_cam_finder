5b9be750f644ae99ecfa68179ce426bb44390a35
kaloqnnajdenov <40794986+kaloqnnajdenov@users.noreply.github.com>
2025-10-11 16:15:34 +0200

    Allow Cyrillic input for segment text fields

--- lib/presentation/pages/create_segment/widgets/segment_labeled_text_field.dart
+++ lib/presentation/pages/create_segment/widgets/segment_labeled_text_field.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SegmentLabeledTextField extends StatelessWidget {
   const SegmentLabeledTextField({
@@ -14,11 +16,20 @@ class SegmentLabeledTextField extends StatelessWidget {
   final String? hintText;
   final FocusNode? focusNode;
 
+  @visibleForTesting
+  static const RegExp allowedCharactersPattern = RegExp(
+    r'[\p{L}\p{M}\p{N}\p{P}\p{S}\p{Zs}]',
+    unicode: true,
+  );
+
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       focusNode: focusNode,
+      inputFormatters: const [
+        FilteringTextInputFormatter.allow(allowedCharactersPattern),
+      ],
       decoration: InputDecoration(
         labelText: label,
         hintText: hintText,
