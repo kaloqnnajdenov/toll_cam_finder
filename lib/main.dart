6a471e871b8229fc370d2314ac08aa6fee805ddc
kaloqnnajdenov <40794986+kaloqnnajdenov@users.noreply.github.com>
2025-10-25 14:30:30 +0200

    Refactor segment tracking to use shared current segment controller

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:toll_cam_finder/app/app.dart';
 import 'package:toll_cam_finder/core/supabase_config.dart';
 import 'package:toll_cam_finder/features/auth/application/auth_controller.dart';
-import 'package:toll_cam_finder/features/map/domain/controllers/average_speed_controller.dart';
 import 'package:toll_cam_finder/features/map/domain/controllers/guidance_audio_controller.dart';
 import 'package:toll_cam_finder/features/map/domain/controllers/segments_only_mode_controller.dart';
+import 'package:toll_cam_finder/features/segments/domain/controllers/current_segment_controller.dart';
 import 'package:toll_cam_finder/shared/services/language_controller.dart';
 import 'package:toll_cam_finder/shared/services/theme_controller.dart';
 import 'package:toll_cam_finder/shared/audio/navigation_audio_context.dart';
@@ -35,10 +35,10 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => AverageSpeedController(),
+          create: (_) => SegmentsOnlyModeController(),
         ),
         ChangeNotifierProvider(
-          create: (_) => SegmentsOnlyModeController(),
+          create: (_) => CurrentSegmentController(),
         ),
         ChangeNotifierProvider(
           create: (_) => AuthController(client: supabaseClient),
